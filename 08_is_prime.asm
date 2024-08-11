55d3592ca04c16b1bc3af2640db7caa2f599bd8e
Ramiro Chacón <ramiro20010202@gmail.com>
2024-08-10 22:58:02 -0600

    is prime

---
+++ 08_is_prime.asm
@@ -0,0 +1,55 @@
+.model small
+.stack 100h
+.data
+                            
+    primo db 'Es primo$'                            
+    noPrimo db 'No es primo$'                
+    numero dw 97                
+
+.code
+
+main:    
+
+    mov ax, @data
+    mov ds, ax
+    
+    mov ax, numero
+    mov bx, ax 
+    mov cx, 1
+    
+    jmp buscar_divisores
+    
+buscar_divisores:
+    inc cx
+    
+    cmp cx, bx
+    je es_primo
+    
+    mov ax, bx
+    
+    div cl
+    
+    cmp ah, 0
+    je no_primo 
+    
+    jmp buscar_divisores 
+    
+es_primo:
+    
+    mov dx, offset primo
+    mov ah, 9
+    int 21h
+    
+    mov ah, 4Ch
+    int 21h     
+    
+no_primo:
+    
+    mov dx, offset noPrimo
+    mov ah, 9
+    int 21h
+    
+    mov ah, 4Ch
+    int 21h                   
+
+end main           
