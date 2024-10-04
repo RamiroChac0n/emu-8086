6bc6dc1f7fedd5a0a9d5309f2cff82391e1f20be
Ramiro Chacón <ramiro20010202@gmail.com>
2024-10-04 08:39:30 -0600

    use asm goto()

--- 02_parcial/03_length.c
+++ 02_parcial/03_length.c
@@ -5,19 +5,38 @@ int longitud = 0;
 int longitudCadena(char *cadena){
 
     asm(
-        "movl $0, %%ecx;"
-        "avanzar:;"
-        "lodsb;"
-        "cmpb $0, %%al;"
-        "je fin;"
-        "stosb;"
-        "incl %%ecx;"
-        "jmp avanzar;" 
-        "fin:;"
-        "movl %%ecx, %[longitud];"       
-        :   "+S" (cadena), "+D" (cadena), [longitud] "=g" (longitud)
+        "movl $-1, %%ecx;"
         :
-        :   "ecx", "al"
+        :
+        :
+    );
+
+    avanzar:
+    asm(
+        "lodsb;"
+        "incl %%ecx;"       
+        "stosb;"       
+        :   "+S" (cadena), "+D" (cadena)
+        :
+        : "ecx", "al"
+    );
+
+    asm goto(
+        "cmpb $0, %%al;"
+        "je %l[fin];"
+        "jmp %l[avanzar];" 
+        :
+        :
+        :   "al"
+        :   avanzar, fin
+    );
+
+    fin:
+    asm(
+        "movl %%ecx, %[longitud];" 
+        : [longitud] "=g" (longitud)
+        :
+        :
     );
     return longitud;
 }
@@ -25,12 +44,11 @@ int longitudCadena(char *cadena){
 int main(void){
 
     char cadena[100];
-    char *ptr = cadena;
 
     printf("Ingrese una cadena: ");
     fgets(cadena, sizeof(cadena), stdin);  
 
-    longitudCadena(ptr);
+    longitudCadena(cadena);
 
     printf("La longitud de la cadena es: %d\n", longitud -1);
 
